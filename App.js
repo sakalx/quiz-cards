c885058f37fa47e26332af6b686d6c45e0e60f9c
sakals <sakals@mail.ua>
2017-12-24 23:31:21 -0600

    Initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {Text, View} from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+        <View>
+          <Text>
+            So far so good !
+          </Text>
+        </View>
+    );
+  }
+}
+
+export default App;
---
+++ constants/Colors.js
@@ -0,0 +1,12 @@
+export default {
+  bodyBackground: '#303030',
+  primary1Color: '#2793e8',
+  primary2Color: '#0097a7',
+  primary3Color: '#757575',
+  alternateTextColor: '#303030',
+  warningColor: '#F44336',
+  errorColor: '#cc181e',
+  accent1Color: '#ff4081',
+  accent2Color: '#f50057',
+  accent3Color: '#ff80ab',
+};
