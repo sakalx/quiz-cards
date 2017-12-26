148ba27439819e33bbe158d1d2cbfa552514cc1c
sakals <sakals@mail.ua>
2017-12-25 21:43:52 -0600

    init: actions types for redux

---
+++ redux/actions/types.js
@@ -0,0 +1,17 @@
+export default {
+  deck: {
+    ALL: 'GET_ALL_DECKS',
+    BY_ID: 'GET_DECK_BY_ID',
+    ADD: 'ADD_DECK',
+    EDIT: 'EDIT_DECK',
+    REMOVE: 'REMOVE_DECK',
+  },
+  card: {
+    ALL: 'GET_ALL_CARDS',
+    BY_ID: 'GET_CARD_BY_ID',
+    BY_DECK: 'GET_CARDS_BY_DECK',
+    ADD: 'ADD_CARD',
+    EDIT: 'EDIT_CARD',
+    REMOVE: 'REMOVE_CARD',
+  },
+};
