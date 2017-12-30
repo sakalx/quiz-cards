c557af3a99eae679f778375fa928e597408c6b10
sakals <sakals@mail.ua>
2017-12-29 21:31:06 -0600

    fix: totally changed structure

--- redux-core/actions/types.js
+++ redux-core/actions/types.js
@@ -4,15 +4,12 @@ export default {
     ALL: 'GET_ALL_DECKS',
     BY_ID: 'GET_DECK_BY_ID',
     ADD: 'ADD_DECK',
-    EDIT: 'EDIT_DECK',
     REMOVE: 'REMOVE_DECK',
+    EDIT: 'EDIT_DECK',
   },
   card: {
-    ALL: 'GET_ALL_CARDS',
-    BY_ID: 'GET_CARD_BY_ID',
-    BY_DECK: 'GET_CARDS_BY_DECK',
     ADD: 'ADD_CARD',
-    EDIT: 'EDIT_CARD',
     REMOVE: 'REMOVE_CARD',
+    EDIT: 'EDIT_CARD',
   },
 };
