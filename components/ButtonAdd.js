c4b964bedac186adc55bc6649806720b73357a89
sakals <sakals@mail.ua>
2018-01-03 10:38:56 -0600

    init: layout button add new Deck

---
+++ components/ButtonAdd.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import palette from 'constants/Colors';
+
+import {TouchableOpacity} from 'react-native';
+
+import {Icon} from 'native-base';
+
+const Button = styled(TouchableOpacity)`
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  bottom: 20;
+  right: 20;
+  width: 52;
+  height: 52;
+  background-color:  ${palette.primary1Color};
+  border-radius: 100;
+`;
+
+const ButtonIcon = styled(Icon)`
+  fontSize: 48;
+  color: #fff;
+`;
+
+class ButtonAdd extends React.Component {
+
+  pressed = () => console.log('puk');
+
+  render() {
+    return (
+        <Button onPress={this.pressed}>
+          <ButtonIcon name='add'/>
+        </Button>
+    )
+  }
+};
+
+export default ButtonAdd;
