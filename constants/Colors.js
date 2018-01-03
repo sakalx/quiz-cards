e71c1c96c1c60ecb18744b10688f42fa58c9b6d1
sakals <sakals@mail.ua>
2018-01-03 10:59:28 -0600

    fix: some playing with colors

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,12 +1,14 @@
 export default {
   bodyBackground: '#303030',
+  alternateBackground: '#3c3c3c',
   primary1Color: '#2793e8',
   primary2Color: '#0097a7',
   primary3Color: '#757575',
-  alternateTextColor: '#303030',
+  alternateTextColor: '#b2b2b2',
   warningColor: '#F44336',
   errorColor: '#cc181e',
   accent1Color: '#ff4081',
   accent2Color: '#f50057',
   accent3Color: '#ff80ab',
+  defaultColor: '#fff',
 };
