30f8f307395fa8e1b814e26cc3f47edb5a9df836
sakals <sakals@mail.ua>
2018-01-05 21:17:27 -0600

    init: App.js

--- App.js
+++ App.js
@@ -1,16 +1,48 @@
-import React from 'react';
+// TODO swiped row for delete or edit + add TOAST
 
-import {Text, View} from 'react-native';
+import React from 'react';
+import {Provider} from 'react-redux';
+import store from './redux-core/store';
+import styled from 'styled-components/native';
+import palette from 'constants/Colors';
+
+import {View} from 'react-native';
+import AppSpinner from 'components/AppSpinner';
+
+import AppNavigator from 'navigation/RootNavigation';
+
+const Spinner = styled(View)`
+ backgroundColor: ${palette.bodyBackground};
+ flex: 1 
+ `;
 
 class App extends React.Component {
+  state = {fontsAreLoaded: false};
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('./assets/fonts/roboto/Roboto.ttf'),
+      'Roboto_medium': require('./assets/fonts/roboto/Roboto_medium.ttf'),
+      'fontello': require('./assets/fonts/fontello/Fontello.ttf'),
+    });
+
+    this.setState({fontsAreLoaded: true});
+  }
+
   render() {
-    return (
-        <View>
-          <Text>
-            So far so good !
-          </Text>
-        </View>
-    );
+    if (this.state.fontsAreLoaded) {
+      return (
+          <Provider store={store}>
+            <AppNavigator/>
+          </Provider>
+      );
+    } else {
+      return (
+          <Spinner>
+            <AppSpinner/>
+          </Spinner>
+      );
+    }
   }
 }
 
