af1ce793f1c5138bd79688ff58021e12fb4d4c0c
sakals <sakals@mail.ua>
2018-01-07 22:40:13 -0600

    feat: handel navigation to new deck screen

--- navigation/RootNavigation.js
+++ navigation/RootNavigation.js
@@ -2,10 +2,12 @@ import {StackNavigator} from 'react-navigation';
 
 import HomeScreen from 'screens/HomeScreen';
 import QuizScreen from 'screens/QuizScreen';
+import NewDeckScreen from 'screens/NewDeckScreen';
 
 const AppNavigator = StackNavigator({
   Home: {screen: HomeScreen},
   Quiz: {screen: QuizScreen},
+  NewDeck: {screen: NewDeckScreen},
 });
 
-export default AppNavigator;
+export default AppNavigator
