717d2c7b91211ba0d87ac29cfb979cf1bd4be116
sakals <sakals@mail.ua>
2018-01-18 21:20:58 -0600

    refactor: changed path

--- navigation/RootNavigation.js
+++ navigation/RootNavigation.js
@@ -2,7 +2,7 @@ import {StackNavigator} from 'react-navigation';
 
 import HomeScreen from 'screens/HomeScreen';
 import QuizScreen from 'screens/QuizScreen';
-import UpdateDeckScreen from 'screens/UpdateDeckScreen';
+import UpdateDeckScreen from 'screens/updateDeck/UpdateDeckScreen';
 
 const AppNavigator = StackNavigator({
   Home: {screen: HomeScreen},
@@ -10,4 +10,4 @@ const AppNavigator = StackNavigator({
   UpdateDeck: {screen: UpdateDeckScreen},
 });
 
-export default AppNavigator
+export default AppNavigator;
