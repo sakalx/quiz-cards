ef05ae954762f8d7fe03c6b1d4f46727c9cdf43f
sakals <sakals@mail.ua>
2018-01-21 22:59:43 -0600

    refactor: trash

--- screens/DeckScreen.js
+++
@@ -1,7 +0,0 @@
-/* TODO
- Individual Deck View
- displays the title of the Deck
- displays the number of cards in the deck
- displays an option to start a quiz on this specific deck
- An option to add a new question to the deck
- */
