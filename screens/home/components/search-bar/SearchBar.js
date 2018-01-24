f4cd73e58a5a07776afd19c60ffb3047c320f964
sakals <sakals@mail.ua>
2018-01-23 21:34:56 -0600

    refactor: separated styles and component

--- screens/home/components/search-bar/SearchBar.js
+++ screens/home/components/search-bar/SearchBar.js
@@ -1,47 +1,20 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import styled from 'styled-components/native';
-import palette from 'constants/Colors';
-import {Constants} from 'expo';
-import {createIconSetFromFontello} from '@expo/vector-icons';
-import fontelloConfig from 'assets/fonts/fontello/fontello.json';
 
-import {Header, Icon, Input, Item} from 'native-base';
-
-const FontelloIcon = createIconSetFromFontello(fontelloConfig);
-
-const Wrap = styled(Header)`
-  background-color: ${palette.bodyBackground};
-  padding-top: ${Constants.statusBarHeight}; 
-  padding-left: 0; 
-  padding-right: 0;
-`;
-const Bar = styled(Item)`
-  background-color: ${palette.alternateBackground};
-  padding: 5px;
-`;
-const SearchField = styled(Input)`
-  background-color: ${palette.alternateBackground};
-  color: #fff;
-`;
-const SearchIcon = styled(Icon)`
- color: ${palette.defaultColor};
-`;
-const DeckIcon = styled(FontelloIcon)`
- color: ${palette.defaultColor};
- font-size: 24;
- `;
+import {Bar, DeckIcon, SearchField, SearchIcon, Wrap} from './style';
 
 @connect(store => ({store}))
 class SearchBar extends React.Component {
 
   handelSearch = query => {
-    const decks = this.props.store.decks;
+    const {updateList, store: {decks}} = this.props;
+    let queryDecks = null;
 
-    const queryDecks = decks.filter(deck =>
-        new RegExp('\\b' + query, 'gi').test(deck.title));
-
-   this.props.updateList(queryDecks);
+    if (query) {
+      queryDecks = decks.filter(deck =>
+          new RegExp('\\b' + query, 'gi').test(deck.title));
+    }
+    updateList(queryDecks);
   };
 
   render() {
@@ -57,7 +30,6 @@ class SearchBar extends React.Component {
         </Wrap>
     );
   }
-
 }
 
 export default SearchBar;
