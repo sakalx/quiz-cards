6af885da173122b08d2d385e753e3befbe28a9c9
sakals <sakals@mail.ua>
2018-01-23 21:37:21 -0600

    style: separated styles and component

--- screens/home/HomeScreen.js
+++ screens/home/HomeScreen.js
@@ -1,70 +1,50 @@
-/* TODO
- Deck List View (Default View)
- displays the title of each Deck
- displays the number of cards in each deck
- */
 import React from 'react';
 import {connect} from 'react-redux';
-import styled from 'styled-components/native';
-import palette from 'constants/Colors';
 import {getAllDecks} from 'redux-core/actions/decks';
-import {showSnack} from 'redux-core/actions/snack';
-
 
 import {FlatList} from 'react-native';
-import {Col, Container, Content, Grid, List, View, Text, Button} from 'native-base';
+import {Container, Content} from 'native-base';
 
 import AppSpinner from 'components/spiner/AppSpinner';
+import Snack from 'components/popup/snack/Snack';
+
 import SearchBar from './components/search-bar/SearchBar';
 import Deck from './components/deck/Deck';
 import ButtonAdd from './components/button-add/ButtonAdd';
-
-
-import Snack from 'components/popup/snack/Snack';
-
-
-const Spinner = styled(View)`
- backgroundColor: ${palette.bodyBackground};
- flex: 1 
- `;
+import {Spinner, wrap} from './style';
 
 @connect(store => ({store}))
 class HomeScreen extends React.Component {
   dispatch = this.props.dispatch;
+
   state = {
     ready: false,
-    decks: [],
+    queryDecks: null,
   };
 
- static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({navigation}) => {
     const {params} = navigation.state;
     const header = <SearchBar updateList={params && params.handleSearch}/>;
-
     return {header};
   };
 
   async componentWillMount() {
-    this.props.navigation.setParams({handleSearch: this.updateListOfDecks});
+    const {navigation, store: {decks}} = this.props;
 
+    navigation.setParams({handleSearch: this.updateListOfDecks});
     await this.dispatch(getAllDecks());
 
     this.setState({
       ready: true,
-      decks: this.props.store.decks,
+      decks,
     });
   }
 
-  updateListOfDecks = (decks = this.props.store.decks) => {
-    
-     this.setState({decks})
-
-  }
-
-
+  updateListOfDecks = queryDecks => this.setState({queryDecks});
 
   render() {
-    const {ready, decks} = this.state;
-    const {navigate} = this.props.navigation;
+    const {ready, queryDecks} = this.state;
+    const {navigation: {navigate}, store: {decks}} = this.props;
 
     if (!ready) {
       return (
@@ -73,30 +53,20 @@ class HomeScreen extends React.Component {
           </Spinner>
       );
     }
- 
+
     return (
         <Container>
-         <Content
-              contentContainerStyle={{backgroundColor: palette.bodyBackground, flex: 1}}>
-
-
- <FlatList
-                    data={decks}
-                    renderItem={({item}) => <Deck deck={item} navigate={navigate}/>}
-
-            
-                    keyExtractor={item => item.id}
-                    numColumns={2}
-                    horizontal={false}
-                />
-
-
-         
+          <Content contentContainerStyle={wrap}>
+            <FlatList
+                data={queryDecks || decks}
+                renderItem={({item}) => <Deck deck={item} navigate={navigate}/>}
+                keyExtractor={item => item.id}
+                numColumns={2}
+                horizontal={false}
+            />
             <ButtonAdd navigate={navigate}/>
           </Content>
-
-
-          <Snack />
+          <Snack/>
         </Container>
     );
   }
