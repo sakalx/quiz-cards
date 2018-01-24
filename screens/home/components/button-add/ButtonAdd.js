57d9f8a69eaaceab310b8b8072c960e4ad2a3445
sakals <sakals@mail.ua>
2018-01-23 21:33:58 -0600

    refactor: separated style and component

--- screens/home/components/button-add/ButtonAdd.js
+++ screens/home/components/button-add/ButtonAdd.js
@@ -1,26 +1,6 @@
 import React from 'react';
-import styled from 'styled-components/native';
-import palette from 'constants/Colors';
 
-import {TouchableOpacity} from 'react-native';
-import {Icon} from 'native-base';
-
-const Button = styled(TouchableOpacity)`
-  align-items: center;
-  justify-content: center;
-  position: absolute;
-  bottom: 20;
-  right: 20;
-  width: 52;
-  height: 52;
-  background-color:  ${palette.primary1Color};
-  border-radius: 100;
-`;
-
-const ButtonIcon = styled(Icon)`
-  fontSize: 48;
-  color: ${palette.defaultColor};
-`;
+import {Button, ButtonIcon} from './style';
 
 class ButtonAdd extends React.Component {
   render() {
