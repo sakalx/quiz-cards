e96bc1f6a3bc618a36005571c5607a3d115164b3
sakals <sakals@mail.ua>
2018-01-23 21:24:29 -0600

    init: drop-down action menu component

---
+++ components/popup/dropdown-actions/ActionsMenu.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import {TouchableOpacity} from 'react-native';
+import {Menu, MenuOptions, MenuTrigger} from 'react-native-popup-menu';
+import {
+  EditIcon,
+  Option,
+  optionsContainer,
+  TitleDelete,
+  TitleEdit,
+  TrashIcon,
+  TriggerIcon
+} from './style.js';
+
+class ActionsMenu extends React.Component {
+  render() {
+    const {style = {}, actions = {}} = this.props;
+
+    return (
+        <Menu style={{...style}}>
+          <MenuTrigger customStyles={{TriggerTouchableComponent: TouchableOpacity}}>
+            <TriggerIcon name='dots-three-vertical'/>
+          </MenuTrigger>
+          <MenuOptions customStyles={{optionsContainer}}>
+            <Option onSelect={() => actions.remove()}>
+              <TitleDelete >DELETE</TitleDelete>
+              <TrashIcon name='trash'/>
+            </Option>
+            <Option onSelect={() => actions.edit()}>
+              <TitleEdit >EDIT</TitleEdit>
+              <EditIcon name='edit'/>
+            </Option>
+          </MenuOptions>
+        </Menu>
+    );
+  }
+}
+
+export default ActionsMenu;
