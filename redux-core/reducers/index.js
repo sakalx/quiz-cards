599684d7a1f29c7f33d14d853b8a79f4c820b85e
sakals <sakals@mail.ua>
2018-01-27 14:18:04 -0600

    feat: new reducer Spinner

--- redux-core/reducers/index.js
+++ redux-core/reducers/index.js
@@ -1,5 +1,6 @@
 import {combineReducers} from 'redux';
 import decks from './decks';
 import snack from './snack';
+import spinner from './spinner';
 
-export default combineReducers({decks, snack});
+export default combineReducers({decks, snack, spinner});
