012724e993d72362bac58b7850c43cb836c2b0ab
sakals <sakals@mail.ua>
2018-01-27 14:12:23 -0600

    feat: added animations

--- screens/home/components/deck/Deck.js
+++ screens/home/components/deck/Deck.js
@@ -1,83 +1,74 @@
 import React from 'react';
-import styled from 'styled-components/native';
-import palette from 'constants/Colors';
-import {createIconSetFromFontello, Entypo} from '@expo/vector-icons';
-import fontelloConfig from 'assets/fonts/fontello/fontello.json';
+import {connect} from 'react-redux';
+import {removeDeck} from 'redux-core/actions/decks';
+import {showSnack} from 'redux-core/actions/snack';
+import snackId from 'components/popup/snack/constants';
 
-import {Text, TouchableOpacity, View, Form, Picker, Item} from 'react-native';
-import {Menu,MenuOptions,MenuOption,  MenuTrigger} from 'react-native-popup-menu';
-import {Icon} from 'native-base';
+import {TouchableOpacity} from 'react-native';
+import * as Animatable from 'react-native-animatable';
 
-import Actions from 'components/popup/dropdown-actions/Actions'
+import ActionsMenu from 'components/popup/dropdown/ActionsMenu';
+import RemoveMessage from 'components/popup/snack/remove-message/RemoveMessage';
 
-const FontelloIcon = createIconSetFromFontello(fontelloConfig);
+import {Badge, BadgeTitle, Card, DeckIcon, menu, Title, Wrap} from './style';
 
+@connect(store => {
+  return {
+    dispatch: store.dispatch,
+  };
+})
 
- const EditIcon = styled(FontelloIcon)`
- color: ${palette.primary1Color};
- font-size: 26;
- margin: 10px;
- `;
-const TrashIcon = styled(Icon)`
- color: ${palette.accent2Color};
- font-size: 26;
- margin: 10px;
- `;
+class Deck extends React.PureComponent {
+  _dispatch = this.props.dispatch;
 
+  async _remove(id) {
+    await this[id].bounceOut(500);
+    this._dispatch(removeDeck(id));
+  }
 
+  handelRemoveDeck(id) {
+    this[id].rubberBand(500);
+    this._dispatch(showSnack({
+      openSnack: snackId.HOME_SCREEN,
+      duration: 3000,
+      content: <RemoveMessage title='DECK'
+                              handelRemove={() => this._remove(id)}
+      />,
+    }));
+  };
 
-const Wrap = styled(View)`
-  align-items: center;
-  margin: 20px 30px;
-`;
-const Card = styled(View)`
-  position: relative;
-`;
-const menu = {
-  position: 'absolute',
-   top: 20, 
-   left: 55
-};
-const Badge = styled(View)`
-  align-items: center;
-  transform: rotate(-8deg);
-  position: absolute;
-  top: 45;
-  right: -45;
-`;
-const BadgeTitle = styled(Text)`
-  color: ${palette.primary2Color};
-`;
-const Title = styled(Text)`
-  font-size: 18;
-  color: ${palette.primary1Color};
-`;
-const DeckIcon = styled(FontelloIcon)`
- font-size: 112;
- `;
-
-class Deck extends React.Component {
   render() {
     const {deck, navigate} = this.props;
     const countQuestions = Object.keys(deck.questions).length;
 
     return (
-        <Wrap>
-          <Card>
-           <Actions style={menu}/>
-            <Badge>
-              <BadgeTitle>{countQuestions}</BadgeTitle>
-              <BadgeTitle>questions</BadgeTitle>
-            </Badge>
-          </Card>
-
-          <TouchableOpacity onPress={() => navigate('Quiz', {deck})}>
-            <DeckIcon name='deck-o'
-                      color={deck.iconColor}
-            />
-          </TouchableOpacity>
-          <Title>{deck.title}</Title>
-        </Wrap>
+        <Animatable.View ref={c => this[deck.id] = c}>
+          <Wrap>
+            <Card>
+              <ActionsMenu style={menu}
+                           actions={{
+                             remove: () => this.handelRemoveDeck(deck.id),
+                             edit: () => navigate('UpdateDeck', {deck}),
+                           }}
+              />
+              <Badge>
+                <BadgeTitle>{countQuestions}</BadgeTitle>
+                <BadgeTitle>
+                  {countQuestions === 1
+                      ? 'question '
+                      : 'questions'
+                  }
+                </BadgeTitle>
+              </Badge>
+            </Card>
+            <TouchableOpacity onPress={() => navigate('Quiz', {deck})}>
+              <DeckIcon name='deck-o'
+                        color={deck.iconColor}
+              />
+            </TouchableOpacity>
+            <Title>{deck.title}</Title>
+          </Wrap>
+        </Animatable.View>
     );
   }
 }
