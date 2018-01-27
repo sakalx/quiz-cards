4bf881226e7841a632e00bba440a9367ddb2bf89
sakals <sakals@mail.ua>
2018-01-27 14:05:56 -0600

    refactor: redone AppBar to stateless component

--- components/app-bar/AppBar.js
+++ components/app-bar/AppBar.js
@@ -3,22 +3,19 @@ import React from 'react';
 import {TouchableOpacity} from 'react-native';
 import {ArrowIcon, Bar, BarTitle, DeckIcon, Wrap} from './style';
 
-class AppBar extends React.Component {
-  render() {
-    const {goBack, state: {params: {deck}}} = this.props.navigation;
-
-    return (
-        <Wrap searchBar>
-          <Bar>
-            <TouchableOpacity onPress={() => goBack()}>
-              <ArrowIcon name='arrow-back'/>
-            </TouchableOpacity>
-            <BarTitle>{deck.title}</BarTitle>
-            <DeckIcon name='deck-o'/>
-          </Bar>
-        </Wrap>
-    );
-  }
-}
+const AppBar = props => {
+  const {goBack, state: {params: {deck}}} = props.navigation;
+  return (
+      <Wrap searchBar>
+        <Bar>
+          <TouchableOpacity onPress={() => goBack()}>
+            <ArrowIcon name='arrow-back'/>
+          </TouchableOpacity>
+          <BarTitle>{deck.title}</BarTitle>
+          <DeckIcon name='deck-o'/>
+        </Bar>
+      </Wrap>
+  );
+};
 
 export default AppBar;
