58722e3d28eda62e6fffe3939d49b630a9bc8e3e
sakals <sakals@mail.ua>
2018-01-27 14:05:11 -0600

    style: typo in word

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import store from 'redux-core/store';
+import {showSpinner} from 'redux-core/actions/spinner';
+
 import styled from 'styled-components/native';
 import palette from 'constants/Colors';
 
@@ -8,7 +10,8 @@ import {View} from 'react-native';
 import {MenuProvider} from 'react-native-popup-menu';
 
 import AppNavigator from 'navigation/RootNavigation';
-import AppSpinner from 'components/spiner/AppSpinner';
+import AppSpinner from 'components/spinner/AppSpinner';
+import spinnerId from 'components/spinner/constants';
 
 const Spinner = styled(View)`
  backgroundColor: ${palette.bodyBackground};
@@ -16,20 +19,23 @@ const Spinner = styled(View)`
  `;
 
 class App extends React.Component {
-  state = {fontsAreLoaded: false};
+  state = {ready: false};
 
   async componentWillMount() {
+    store.dispatch(showSpinner(spinnerId.FONTS_LOADING, true));
+
     await Expo.Font.loadAsync({
       'Roboto': require('./assets/fonts/roboto/Roboto.ttf'),
       'Roboto_medium': require('./assets/fonts/roboto/Roboto_medium.ttf'),
       'fontello': require('./assets/fonts/fontello/Fontello.ttf'),
     });
 
-    this.setState({fontsAreLoaded: true});
+    store.dispatch(showSpinner(spinnerId.FONTS_LOADING, false));
+    this.setState({ready: true});
   }
 
   render() {
-    if (!this.state.fontsAreLoaded) {
+    if (!this.state.ready) {
       return (
           <Spinner>
             <AppSpinner/>
