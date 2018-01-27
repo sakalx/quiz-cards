14e842af02e54f4c43ee1c85115b5bf7c14e4c65
sakals <sakals@mail.ua>
2018-01-27 14:25:32 -0600

    init: SetAnswer component

---
+++ screens/update-deck/components/set-answer/SetAnswer.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {showSnack} from 'redux-core/actions/snack';
+import snackId from 'components/popup/snack/constants';
+
+import {Text, TouchableOpacity} from 'react-native';
+import Modal from 'react-native-modal';
+
+import {ButtonTitle, LogoIcon, SubTitle, Title, Wrap, WrapButtons} from './style';
+
+@connect(store => {
+  return {
+    dispatch: store.dispatch,
+  };
+})
+
+class SetAnswer extends React.PureComponent {
+  _dispatch = this.props.dispatch;
+
+  handelAddQuestion = answer => {
+    const {question, questionId, handelAddQuestionCB} = this.props;
+    const id = questionId || (+new Date()).toString(16);
+
+    const SnackMsg =
+        <Text>
+          successfully {questionId ? 'update' : 'added'}
+        </Text>;
+
+    this._dispatch(showSnack({
+      openSnack: snackId.QUESTION_TAB,
+      content: SnackMsg,
+    }));
+    handelAddQuestionCB(id, question, answer);
+  };
+
+  render() {
+    const {setAnswer, question} = this.props;
+
+    return (
+        <Modal isVisible={setAnswer}
+               animationIn={'slideInLeft'}
+               animationOut={'slideOutRight'}
+        >
+          <Wrap>
+            <Title>
+              {question}
+            </Title>
+            <SubTitle>
+              what should be an answer?
+            </SubTitle>
+            <WrapButtons>
+              <TouchableOpacity onPress={() => this.handelAddQuestion(false)}>
+                <ButtonTitle>
+                  FALSE
+                </ButtonTitle>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => this.handelAddQuestion(true)}>
+                <ButtonTitle>
+                  TRUE
+                </ButtonTitle>
+              </TouchableOpacity>
+            </WrapButtons>
+            <LogoIcon name='logo'/>
+          </Wrap>
+        </Modal>
+    );
+  }
+}
+
+export default SetAnswer;
